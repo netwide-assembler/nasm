f8ab4baba131d231b74dfa868e50c0b52a3ccacd
H. Peter Anvin <hpa@zytor.com>
2002-05-22 23:23:02 +0000

    Change space to NBSP to avoid breakage on 'doze platforms

--- doc/genpsdriver.pl
+++ doc/genpsdriver.pl
@@ -24,4 +24,6 @@ $version = <VERSION>;
 chomp $version;
 close(VERSION);
 
-system($perl, $genps, '-subtitle', 'version '.$version, 'nasmdoc.dip');
+# \240 = no-break space, see @NASMEncoding in genps.pl.
+# If we use a normal space, it breaks on 'doze platforms...
+system($perl, $genps, '-subtitle', "version\240".$version, 'nasmdoc.dip');
