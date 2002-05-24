591553d88dccc6bfdb36e06aabc0f2a2aea06dc6
Debbie Wiles <debs@dwiles.demon.co.uk>
2002-05-24 14:33:35 +0000

    Added extra tests, to fully test multisection support for .bin format

--- test/multisection.asm
+++ test/multisection.asm
@@ -1,3 +1,22 @@
+; To test where code that is placed before any explicit SECTION
+; gets placed, and what happens if a .text section has an ORG
+;statement, uncomment the following lines.
+;
+;	times	10h	nop
+;
+;section .text
+;org 0x300
+;	times	20h	inc ax
+
+; let's see which of these sections can be placed in the specified order.
+
+section .appspecific
+section .data
+section .stringdata
+section .mytext
+section .code
+section .extra_code
+
 
 section .stringdata
 mystr1: db "Hello, this is string 1", 13, 10, '$'
@@ -40,3 +59,28 @@ start:
 
    jmp more
 
+section .text
+	xor	eax,eax
+	times	50h nop
+
+section .mytext
+
+	xor	ebx,ebx
+
+section .data
+	db	95h,95h,95h,95h,95h,95h,95h,95h
+
+section .hmm
+	resd	2
+
+section .bss
+	resd	8
+
+section .final1
+	inc	ax
+
+section .final2
+	inc	bx
+
+section .final3
+	inc	cx
