0a76a9e69069cc45a44fcec08c7f52932cdbc0f4
Stanislav Karchebny <berkus@users.sourceforge.net>
2003-09-26 04:20:30 +0000

    Backed out another patch =)

--- nasm.c
+++ nasm.c
@@ -185,7 +185,7 @@ int main(int argc, char **argv)
     /* define some macros dependent of command-line */
     {
 	char temp [64];
-	sprintf (temp, "__OUTPUT_FORMAT__='%s'\n", ofmt->shortname);
+	sprintf (temp, "__OUTPUT_FORMAT__=%s\n", ofmt->shortname);
 	pp_pre_define (temp);
     }
 
