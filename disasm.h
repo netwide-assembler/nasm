b30ff1f2846e709e86896517f93ceed9de2f69e3
Ed Beroset <beroset@mindspring.com>
2004-12-15 18:26:37 +0000

    added buffer length parameters to prevent vulnerability to buffer overflow exploits.

--- disasm.h
+++ disasm.h
@@ -11,8 +11,8 @@
 
 #define INSN_MAX 32	/* one instruction can't be longer than this */
 
-long disasm (unsigned char *data, char *output, int segsize, long offset,
-	     int autosync, unsigned long prefer);
-long eatbyte (unsigned char *data, char *output);
+long disasm (unsigned char *data, char *output, int outbufsize, int segsize, 
+	     long offset, int autosync, unsigned long prefer);
+long eatbyte (unsigned char *data, char *output, int outbufsize);
 
 #endif
