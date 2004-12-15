602f1df356d8ca6b8664f8b4728e6fcd162763b3
Ed Beroset <beroset@mindspring.com>
2004-12-15 18:27:21 +0000

    added buffer length parameter to prevent vulnerability to buffer overflow exploits.

--- ndisasm.c
+++ ndisasm.c
@@ -243,10 +243,10 @@ int main(int argc, char **argv)
 	    nextsync = next_sync (offset, &synclen);
 	}
 	while (p > q && (p - q >= INSN_MAX || lenread == 0)) {
-	    lendis = disasm (q, outbuf, bits, offset, autosync, prefer);
+	    lendis = disasm (q, outbuf, sizeof(outbuf), bits, offset, autosync, prefer);
 	    if (!lendis || lendis > (p - q) ||
 		(unsigned long)lendis > nextsync-offset)
-		lendis = eatbyte (q, outbuf);
+		lendis = eatbyte (q, outbuf, sizeof(outbuf));
 	    output_ins (offset, q, lendis, outbuf);
 	    q += lendis;
 	    offset += lendis;
