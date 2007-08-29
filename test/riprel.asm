859f085985563e2b7fb734277ed23b338b128e10
H. Peter Anvin <hpa@zytor.com>
2007-08-29 16:38:47 +0000

    More test cases for rel and abs addressing

--- test/riprel.asm
+++ test/riprel.asm
@@ -1,5 +1,7 @@
 	bits 64
-;	extern foo
+
+	[default abs]
+	
 	mov rax,[foo]
 	mov rax,[123456789abcdef0h]
 	mov rbx,[foo]
@@ -10,6 +12,10 @@
 	mov rbx,[rel foo]
 	mov rax,[rel dword foo]
 	mov rax,[rel qword foo]
+	mov rax,[abs foo]
+	mov rbx,[abs foo]
+	mov rax,[abs dword foo]
+	mov rax,[abs qword foo]
 
 	mov rax,[es:foo]
 	mov rax,[es:123456789abcdef0h]
@@ -21,6 +27,10 @@
 	mov rbx,[rel es:foo]
 	mov rax,[rel dword es:foo]
 	mov rax,[rel qword es:foo]
+	mov rax,[abs es:foo]
+	mov rbx,[abs es:foo]
+	mov rax,[abs dword es:foo]
+	mov rax,[abs qword es:foo]
 
 	mov rax,[fs:foo]
 	mov rax,[fs:123456789abcdef0h]
@@ -32,6 +42,57 @@
 	mov rbx,[rel fs:foo]
 	mov rax,[rel dword fs:foo]
 	mov rax,[rel qword fs:foo]
+	mov rax,[abs fs:foo]
+	mov rbx,[abs fs:foo]
+	mov rax,[abs dword fs:foo]
+	mov rax,[abs qword fs:foo]
+
+	[default rel]
+	
+	mov rax,[foo]
+	mov rax,[123456789abcdef0h]
+	mov rbx,[foo]
+	mov rax,[dword foo]
+	mov rbx,[dword foo]
+	mov rax,[qword foo]
+	mov rax,[rel foo]
+	mov rbx,[rel foo]
+	mov rax,[rel dword foo]
+	mov rax,[rel qword foo]
+	mov rax,[abs foo]
+	mov rbx,[abs foo]
+	mov rax,[abs dword foo]
+	mov rax,[abs qword foo]
+
+	mov rax,[es:foo]
+	mov rax,[es:123456789abcdef0h]
+	mov rbx,[es:foo]
+	mov rax,[dword es:foo]
+	mov rbx,[dword es:foo]
+	mov rax,[qword es:foo]
+	mov rax,[rel es:foo]
+	mov rbx,[rel es:foo]
+	mov rax,[rel dword es:foo]
+	mov rax,[rel qword es:foo]
+	mov rax,[abs es:foo]
+	mov rbx,[abs es:foo]
+	mov rax,[abs dword es:foo]
+	mov rax,[abs qword es:foo]
+
+	mov rax,[fs:foo]
+	mov rax,[fs:123456789abcdef0h]
+	mov rbx,[fs:foo]
+	mov rax,[dword fs:foo]
+	mov rbx,[dword fs:foo]
+	mov rax,[qword fs:foo]
+	mov rax,[rel fs:foo]
+	mov rbx,[rel fs:foo]
+	mov rax,[rel dword fs:foo]
+	mov rax,[rel qword fs:foo]
+	mov rax,[abs fs:foo]
+	mov rbx,[abs fs:foo]
+	mov rax,[abs dword fs:foo]
+	mov rax,[abs qword fs:foo]
 
 	section .data
 foo	equ $
