e30393639145a6ed8c599b8dc112e662a5754d88
H. Peter Anvin <hpa@zytor.com>
2008-05-06 20:06:14 -0700

    Fix encoding of VPERMIL2PS instructions

--- test/avx.asm
+++ test/avx.asm
@@ -1,14 +1,7 @@
 		bits 64
 		vpermil2ps	xmm0,xmm1,[rdi],xmm3,0
-		times 16 nop
 		vpermil2ps	xmm0,xmm1,xmm2,[rdi],1
-		times 16 nop
 		vpermil2ps	ymm0,ymm1,ymm2,ymm3,2
-		times 16 nop
 		vpermil2ps	ymm0,ymm1,ymm2,[rdi],3
-		times 16 nop
 		vpermil2ps	ymm0,ymm1,[rdi],ymm3,2
-		times 16 nop
 		vpermil2ps	ymm0,ymm1,ymm2,[rdi],3
-		times 16 nop
-
