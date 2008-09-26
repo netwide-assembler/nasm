0819e3b9a726c8ff905670409504b15d0f569cbf
H. Peter Anvin <hpa@zytor.com>
2008-09-25 23:45:20 -0700

    Add more 64-bit jump tests

--- test/jmp64.asm
+++ test/jmp64.asm
@@ -1,4 +1,15 @@
+;Testname=test; Arguments=-fbin -ojmp64.bin; Files=.stdout .stderr jmp64.bin
+
 	bits 64
 	jmp rcx
+	jmp [rax]
+	jmp qword [rax]
+	jmp far [rax]
+	jmp far dword [rax]
+	jmp far qword [rax]
+	call rcx
 	call [rax]
-	call qword [rdx]
+	call qword [rax]
+	call far [rax]
+	call far dword [rax]
+	call far qword [rax]
