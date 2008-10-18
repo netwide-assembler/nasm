e41b69beaf51a725a98b1ba4480751333b24a06f
H. Peter Anvin <hpa@zytor.com>
2008-10-17 17:13:26 -0700

    Test and Makefile rules for 32- and 64-bit ELF shared libraries
    Add Makefile rules for the 32-bit ELF shared library test, and add a
    64-bit ELF shared library test (still work in progress.)

--- test/elftest64.c
+++ test/elftest64.c
@@ -1,33 +1,37 @@
 /*
- * test source file for assembling to ELF
- * build with:
- *    nasm -f elf elftest.asm
- *    gcc -o elftest elftest.c elftest.o
- * (assuming your gcc is ELF)
+ *  build with:
+ *	nasm -f elf64 elf64so.asm
+ *	ld -shared -o elf64so.so elf64so.o
+ * test with:
+ *	gcc -o elf64so elftest64.c ./elf64so.so
+ *	./elf64so
  */
 
 #include <stdio.h>
 #include <inttypes.h>
 
-extern int lrotate(int32_t, int);
+extern long lrotate(long, int);
 extern void greet(void);
 extern int8_t asmstr[];
 extern void *selfptr;
 extern void *textptr;
-extern int integer, commvar;
+extern long integer;
+long commvar;
 
 int main(void)
 {
 
     printf("Testing lrotate: should get 0x00400000, 0x00000001\n");
     printf("lrotate(0x00040000, 4) = 0x%08lx\n", lrotate(0x40000, 4));
-    printf("lrotate(0x00040000, 14) = 0x%08lx\n", lrotate(0x40000, 14));
+    printf("lrotate(0x00040000, 46) = 0x%08lx\n", lrotate(0x40000, 46));
 
     printf("This string should read `hello, world': `%s'\n", asmstr);
 
+    printf("&integer = %p, &commvar = %p\n", &integer, &commvar);
     printf("The integers here should be 1234, 1235 and 4321:\n");
     integer = 1234;
     commvar = 4321;
+
     greet();
 
     printf("These pointers should be equal: %p and %p\n", &greet, textptr);
