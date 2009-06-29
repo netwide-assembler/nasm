c8578cec67eb55266f195b9c379deb8d99a1b80a
H. Peter Anvin <hpa@zytor.com>
2009-06-28 17:33:50 -0700

    Add new copyright headers to the rdoff code
    To the best of my knowledge, the only file which we don't have 2-BSD
    relicensing permission for at this stage is rdf2ihx.c.  That file
    probably should just be rewritten, if anyone cares.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- rdoff/ldrdf.c
+++ rdoff/ldrdf.c
@@ -1,12 +1,38 @@
+/* ----------------------------------------------------------------------- *
+ *   
+ *   Copyright 1996-2009 The NASM Authors - All Rights Reserved
+ *   See the file AUTHORS included with the NASM distribution for
+ *   the specific copyright holders.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following
+ *   conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *     
+ *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *     CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *     SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *     NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *     HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *     OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ----------------------------------------------------------------------- */
+
 /*
  * ldrdf.c - RDOFF Object File linker/loader main program.
- *
- * Copyright (c) 1996,99 Julian Hall. All rights reserved.
- * Improvements and fixes (c) 1999-2004 RET & COM Research.
- *
- * This file is distributed under the terms and conditions of the
- * GNU Lesser Public License (LGPL), version 2.1.
- * See http://www.gnu.org/copyleft/lgpl.html for details.
  */
 
 /*
