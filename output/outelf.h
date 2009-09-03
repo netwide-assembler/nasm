2cff224e4bbec89e6119ee1245ce818c83091857
Cyrill Gorcunov <gorcunov@gmail.com>
2009-09-03 19:04:23 +0400

    output/outelf.h - tab-space cleanup
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- output/outelf.h
+++ output/outelf.h
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------- *
- *   
+ *
  *   Copyright 1996-2009 The NASM Authors - All Rights Reserved
  *   See the file AUTHORS included with the NASM distribution for
  *   the specific copyright holders.
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *     
+ *
  *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
  *     CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  *     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -57,40 +57,40 @@
 
 /* Known sections with nonstandard defaults */
 struct elf_known_section {
-    const char *name;		/* Name of section */
-    int type;			/* Section type (SHT_) */
-    uint32_t flags;		/* Section flags (SHF_) */
-    uint32_t align;		/* Section alignment */
+    const char *name;   /* Name of section */
+    int type;           /* Section type (SHT_) */
+    uint32_t flags;     /* Section flags (SHF_) */
+    uint32_t align;     /* Section alignment */
 };
 extern const struct elf_known_section elf_known_sections[];
 
 /*
  * Special ELF sections (after the real sections but before debugging ones)
  */
-#define sec_shstrtab		(nsects + 1)
-#define sec_symtab		(nsects + 2)
-#define sec_strtab		(nsects + 3)
-#define sec_numspecial  	3
+#define sec_shstrtab            (nsects + 1)
+#define sec_symtab              (nsects + 2)
+#define sec_strtab              (nsects + 3)
+#define sec_numspecial          3
 
 /*
  * Debugging ELF sections (last in the file)
  */
 
 /* stabs */
-#define sec_stab		(nsections-3)
-#define sec_stabstr		(nsections-2)
-#define sec_rel_stab		(nsections-1)
+#define sec_stab                (nsections-3)
+#define sec_stabstr             (nsections-2)
+#define sec_rel_stab            (nsections-1)
 
 /* dwarf */
-#define sec_debug_aranges	(nsections-10)
-#define sec_rela_debug_aranges	(nsections-9)
-#define sec_debug_pubnames	(nsections-8)
-#define sec_debug_info		(nsections-7)
-#define sec_rela_debug_info	(nsections-6)
-#define sec_debug_abbrev	(nsections-5)
-#define sec_debug_line		(nsections-4)
-#define sec_rela_debug_line	(nsections-3)
-#define sec_debug_frame		(nsections-2)
-#define sec_debug_loc		(nsections-1)
+#define sec_debug_aranges       (nsections-10)
+#define sec_rela_debug_aranges  (nsections-9)
+#define sec_debug_pubnames      (nsections-8)
+#define sec_debug_info          (nsections-7)
+#define sec_rela_debug_info     (nsections-6)
+#define sec_debug_abbrev        (nsections-5)
+#define sec_debug_line          (nsections-4)
+#define sec_rela_debug_line     (nsections-3)
+#define sec_debug_frame         (nsections-2)
+#define sec_debug_loc           (nsections-1)
 
 #endif /* OUTPUT_OUTELF_H */
