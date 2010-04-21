d71d876f4671964b8219f1e71e1eeff5354bda2b
Cyrill Gorcunov <gorcunov@gmail.com>
2010-04-21 21:47:03 +0400

    pecoff.h: Add more constants
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- output/pecoff.h
+++ output/pecoff.h
@@ -65,6 +65,52 @@
 #define IMAGE_FILE_MACHINE_MASK         0xffff
 
 /*
+ * Characteristics
+ */
+#define IMAGE_FILE_RELOCS_STRIPPED              0x0001
+#define IMAGE_FILE_EXECUTABLE_IMAGE             0x0002
+#define IMAGE_FILE_LINE_NUMS_STRIPPED           0x0004
+#define IMAGE_FILE_LOCAL_SYMS_STRIPPED          0x0008
+#define IMAGE_FILE_AGGRESSIVE_WS_TRIM           0x0010
+#define IMAGE_FILE_LARGE_ADDRESS_AWARE          0x0020
+#define IMAGE_FILE_BYTES_REVERSED_LO            0x0080
+#define IMAGE_FILE_32BIT_MACHINE                0x0100
+#define IMAGE_FILE_DEBUG_STRIPPED               0x0200
+#define IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP      0x0400
+#define IMAGE_FILE_NET_RUN_FROM_SWAP            0x0800
+#define IMAGE_FILE_SYSTEM                       0x1000
+#define IMAGE_FILE_DLL                          0x2000
+#define IMAGE_FILE_UP_SYSTEM_ONLY               0x4000
+#define IMAGE_FILE_BYTES_REVERSED_HI            0x8000
+
+/*
+ * Windows subsystem
+ */
+#define IMAGE_SUBSYSTEM_UNKNOWN                 0
+#define IMAGE_SUBSYSTEM_NATIVE                  1
+#define IMAGE_SUBSYSTEM_WINDOWS_GUI             2
+#define IMAGE_SUBSYSTEM_WINDOWS_CUI             3
+#define IMAGE_SUBSYSTEM_POSIX_CUI               7
+#define IMAGE_SUBSYSTEM_WINDOWS_CE_GUI          9
+#define IMAGE_SUBSYSTEM_EFI_APPLICATION         10
+#define IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER 11
+#define IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER      12
+#define IMAGE_SUBSYSTEM_EFI_ROM                 13
+#define IMAGE_SUBSYSTEM_XBOX                    14
+
+/*
+ * DLL characteristics
+ */
+#define IMAGE_DLL_CHARACTERISTICS_DYNAMIC_BASE          0x0040
+#define IMAGE_DLL_CHARACTERISTICS_FORCE_INTEGRITY       0x0080
+#define IMAGE_DLL_CHARACTERISTICS_NX_COMPAT             0x0100
+#define IMAGE_DLLCHARACTERISTICS_NO_ISOLATION           0x0200
+#define IMAGE_DLLCHARACTERISTICS_NO_SEH                 0x0400
+#define IMAGE_DLLCHARACTERISTICS_NO_BIND                0x0800
+#define IMAGE_DLLCHARACTERISTICS_WDM_DRIVER             0x2000
+#define IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE  0x8000
+
+/*
  * Section flags
  */
 #define IMAGE_SCN_TYPE_REG                      0x00000000
@@ -114,5 +160,49 @@
 #define IMAGE_SCN_MEM_EXECUTE                   0x20000000
 #define IMAGE_SCN_MEM_READ                      0x40000000
 #define IMAGE_SCN_MEM_WRITE                     0x80000000
+
+/*
+ * Relocation type x86-64
+ */
+#define IMAGE_REL_AMD64_ABSOLUTE        0x0000
+#define IMAGE_REL_AMD64_ADDR64          0x0001
+#define IMAGE_REL_AMD64_ADDR32          0x0002
+#define IMAGE_REL_AMD64_ADDR32NB        0x0003
+#define IMAGE_REL_AMD64_REL32           0x0004
+#define IMAGE_REL_AMD64_REL32_1         0x0005
+#define IMAGE_REL_AMD64_REL32_2         0x0006
+#define IMAGE_REL_AMD64_REL32_3         0x0007
+#define IMAGE_REL_AMD64_REL32_4         0x0008
+#define IMAGE_REL_AMD64_REL32_5         0x0009
+#define IMAGE_REL_AMD64_SECTION         0x000a
+#define IMAGE_REL_AMD64_SECREL          0x000b
+#define IMAGE_REL_AMD64_SECREL7         0x000c
+#define IMAGE_REL_AMD64_TOKEN           0x000d
+#define IMAGE_REL_AMD64_SREL32          0x000e
+#define IMAGE_REL_AMD64_PAIR            0x000f
+#define IMAGE_REL_AMD64_SSPAN32         0x0010
+
+/*
+ * Relocation types i386
+ */
+#define IMAGE_REL_I386_ABSOLUTE         0x0000
+#define IMAGE_REL_I386_DIR16            0x0001
+#define IMAGE_REL_I386_REL16            0x0002
+#define IMAGE_REL_I386_DIR32            0x0006
+#define IMAGE_REL_I386_DIR32NB          0x0007
+#define IMAGE_REL_I386_SEG12            0x0009
+#define IMAGE_REL_I386_SECTION          0x000a
+#define IMAGE_REL_I386_SECREL           0x000b
+#define IMAGE_REL_I386_TOKEN            0x000c
+#define IMAGE_REL_I386_SECREL7          0x000d
+#define IMAGE_REL_I386_REL32            0x0014
+
+/*
+ * TODO: Add other archs here?
+ */
+
+/*
+ * TODO: Append the rest definitions from spec
+ */
 
 #endif /* PECOFF_H */
