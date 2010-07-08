ddd08c3cccb4b68ecdb24d7a92eab6b2b82e8c68
H. Peter Anvin <hpa@zytor.com>
2010-07-08 08:33:14 -0700

    Revert "expand_mmac_params: Expand local single macros unconditionally"
    This reverts commit 985d880c15a5b26e59cdcec4af2eba0748ecfe1f.
    
    Revert due to BR 3026808.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- preproc.c
+++ preproc.c
@@ -3822,7 +3822,14 @@ static Token *expand_mmac_params(Token * tline)
             changed = true;
         } else if (tline->type == TOK_PREPROC_ID &&
                    tline->text[0] == '%' && tline->text[1] == '$') {
-            /* expand local macro */
+            /*
+             * In a sake of backward compatibility we allow
+             * to expand local single macro that early before
+             * pasting token code have place
+             *
+             * NOTE: that new code MUST use %+ macro to obtain
+             * same result
+             */
             t = tline;
             tline = tline->next;
             tt = tokenize(t->text);
