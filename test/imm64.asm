acf1cbb25077c64bff72a51785c1ef2134c8e885
Cyrill Gorcunov <gorcunov@gmail.com>
2010-08-04 17:27:17 +0400

    test: Add automatizing annotations to imm64.asm
    H. Peter Anvin pointed
    |
    | Btw, test/imm64.asm needs test engine annotations.
    |
    
    Make it so.
    
    Reported-by: "H. Peter Anvin" <hpa@zytor.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- test/imm64.asm
+++ test/imm64.asm
@@ -1,3 +1,7 @@
+;Testname=imm64-O0; Arguments=-O0 -fbin -oimm64.bin; Files=stdout stderr imm64.bin
+;Testname=imm64-O1; Arguments=-O1 -fbin -oimm64.bin; Files=stdout stderr imm64.bin
+;Testname=imm64-Ox; Arguments=-Ox -fbin -oimm64.bin; Files=stdout stderr imm64.bin
+
 	bits 64
 	mov rax,11223344h
 	mov rax,dword 11223344h
