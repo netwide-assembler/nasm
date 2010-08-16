dbdb6d3df6a21e6d6e826ae0fe57c61dc1e4e0d3
H. Peter Anvin <hpa@linux.intel.com>
2010-08-16 15:23:16 -0700

    test/avx: remove deleted instructions
    Remove the deleted VPERMIL2 instructions.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- test/avx.asm
+++ test/avx.asm
@@ -14,13 +14,6 @@
 		vcvtsi2sd	xmm9,xmm10,dword [rdi]
 		vcvtsi2sd	xmm9,xmm10,qword [rdi]
 
-		vpermil2ps	xmm0,xmm1,[rdi],xmm3,0
-		vpermil2ps	xmm0,xmm1,xmm2,[rdi],1
-		vpermil2ps	ymm0,ymm1,ymm2,ymm3,2
-		vpermil2ps	ymm0,ymm1,ymm2,[rdi],3
-		vpermil2ps	ymm0,ymm1,[rdi],ymm3,2
-		vpermil2ps	ymm0,ymm1,ymm2,[rdi],3
-
 		vpextrb		[rax],xmm1,0x33
 		vpextrw		[rax],xmm1,0x33
 		vpextrd		[rax],xmm1,0x33
