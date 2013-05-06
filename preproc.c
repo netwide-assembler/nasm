ce6527459f0144a57fc628e0c978594daf1db281
Cyrill Gorcunov <gorcunov@gmail.com>
2013-05-06 23:43:43 +0400

    BR3392253: Fix potential buffer overflow in number conversion
    Reported-by: franck.uberto@esrf.fr
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- preproc.c
+++ preproc.c
@@ -5206,7 +5206,7 @@ static void pp_extra_stdmac(macros_t *macros)
 
 static void make_tok_num(Token * tok, int64_t val)
 {
-    char numbuf[20];
+    char numbuf[32];
     snprintf(numbuf, sizeof(numbuf), "%"PRId64"", val);
     tok->text = nasm_strdup(numbuf);
     tok->type = TOK_NUMBER;
