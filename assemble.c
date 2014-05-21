0a9250c2ab32028dfdca6578655882b4be6a5c04
H. Peter Anvin <hpa@zytor.com>
2014-05-21 08:19:16 -0700

    BR 3392279: Fix duplicated REX prefixes
    The fix for BR 3392278:
    
    aa29b1d93f5a assemble.c: Don't drop rex prefix from instruction itself
    
    ... would cause multiple REX prefixes to be emitted for some
    instructions.  Create a new flag to indicate that REX has already been
    emitted, which can be cleared for each instance of an instruction.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- assemble.c
+++ assemble.c
@@ -1,6 +1,6 @@
 /* ----------------------------------------------------------------------- *
  *
- *   Copyright 1996-2013 The NASM Authors - All Rights Reserved
+ *   Copyright 1996-2014 The NASM Authors - All Rights Reserved
  *   See the file AUTHORS included with the NASM distribution for
  *   the specific copyright holders.
  *
@@ -1365,9 +1365,12 @@ static int64_t calcsize(int32_t segment, int64_t offset, int bits,
 static inline unsigned int emit_rex(insn *ins, int32_t segment, int64_t offset, int bits)
 {
     if (bits == 64) {
-        if ((ins->rex & REX_REAL) && !(ins->rex & (REX_V | REX_EV))) {
+        if ((ins->rex & REX_REAL) &&
+            !(ins->rex & (REX_V | REX_EV)) &&
+            !ins->rex_done) {
             int rex = (ins->rex & REX_REAL) | REX_P;
             out(offset, segment, &rex, OUT_RAWDATA, 1, NO_SEG, NO_SEG);
+            ins->rex_done = true;
             return 1;
         }
     }
@@ -1388,6 +1391,8 @@ static void gencode(int32_t segment, int64_t offset, int bits,
     const uint8_t *codes = temp->code;
     uint8_t opex = 0;
     enum ea_type eat = EA_SCALAR;
+
+    ins->rex_done = false;
 
     while (*codes) {
         c = *codes++;
