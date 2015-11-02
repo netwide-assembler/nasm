be8a5c8f2dbde47719796209a796cd7ccea32e54
Mark Scott <nasm@mscott.cx>
2015-11-03 01:13:30 +0300

    out: elf,stabs -- Fix @n_value attribute for HdrSym entry
    Each stabs entry is 12 bytes in size, for some reason we've
    been pasing wrong attribute here in @n_value.
    
    Signed-off-by: Mark Scott <nasm@mscott.cx>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- output/outelf64.c
+++ output/outelf64.c
@@ -1682,7 +1682,7 @@ static void stabs64_generate(void)
          * the source-file, the n_desc field should be set to the number
          * of remaining stabs
          */
-        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, strlen(allfiles[0] + 12));
+        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, stabstrlen);
 
         /* this is the stab for the main source file */
         WRITE_STAB(sptr, fileidx[mainfileindex], N_SO, 0, 0, 0);
--- output/outelfx32.c
+++ output/outelfx32.c
@@ -1641,7 +1641,7 @@ static void stabsx32_generate(void)
          * the source-file, the n_desc field should be set to the number
          * of remaining stabs
          */
-        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, strlen(allfiles[0] + 12));
+        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, stabstrlen);
 
         /* this is the stab for the main source file */
         WRITE_STAB(sptr, fileidx[mainfileindex], N_SO, 0, 0, 0);
