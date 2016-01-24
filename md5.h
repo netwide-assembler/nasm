ba754eec038dd419d64ada943af4476202d5af25
Jim Kukunas <james.t.kukunas@linux.intel.com>
2016-01-23 23:32:50 -0800

    Add a MD5 implementation
    This implementation was written by Colin Plumb and
    is in the public domain.
    
    I've updated it to use stdint.h and the standard C types rather than
    sys/types.h for portability.
    
    Signed-off-by: Jim Kukunas <james.t.kukunas@linux.intel.com>
    Acked-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

---
+++ md5.h
@@ -0,0 +1,23 @@
+#ifndef _MD5_H_
+#define _MD5_H_
+
+#include <stdint.h>
+
+#define MD5_HASHBYTES 16
+
+typedef struct MD5Context {
+	uint32_t buf[4];
+	uint32_t bits[2];
+	unsigned char in[64];
+} MD5_CTX;
+
+extern void   MD5Init(MD5_CTX *context);
+extern void   MD5Update(MD5_CTX *context, unsigned char const *buf,
+	       unsigned len);
+extern void   MD5Final(unsigned char digest[MD5_HASHBYTES], MD5_CTX *context);
+extern void   MD5Transform(uint32_t buf[4], uint32_t const in[16]);
+extern char * MD5End(MD5_CTX *, char *);
+extern char * MD5File(const char *, char *);
+extern char * MD5Data (const unsigned char *, unsigned int, char *);
+
+#endif /* !_MD5_H_ */
