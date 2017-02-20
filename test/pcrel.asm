ed71316e2bf64d77b57b93880f8ae384cf407da0
H. Peter Anvin <hpa@linux.intel.com>
2017-02-20 13:06:02 -0800

    test/pcrel.asm: make test output a bit easier to understand
    Make the output of the test a bit easier to pick apart.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- test/pcrel.asm
+++ test/pcrel.asm
@@ -19,24 +19,26 @@ foo:				; Backwards reference
 	bits 64
 	default rel
 
+	times 16 nop
+
 	mov eax,[foo]
 	mov eax,[foo - $]
 	mov eax,[abs foo - $]
-	mov ebx,[ebx + foo - $]
+	mov ebx,[rbx + foo - $]
 	mov ecx,foo - $
 	mov edx,foo - bar
 
 	mov eax,[bar]
 	mov eax,[bar - $]
 	mov eax,[abs bar - $]
-	mov ebx,[ebx + bar - $]
+	mov ebx,[rbx + bar - $]
 	mov ecx,bar - $
 	mov edx,bar - foo
 
 	mov eax,[baz]
 	mov eax,[baz - $]
 	mov eax,[abs baz - $]
-	mov ebx,[ebx + baz - $]
+	mov ebx,[rbx + baz - $]
 	mov esi,[baz - bar]
 	mov esi,[abs baz - bar]
 	mov ecx,baz - $
