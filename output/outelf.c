230db148a32468468ee5f4979c6b08b0848e1e22
H. Peter Anvin <hpa@linux.intel.com>
2017-09-27 15:12:03 -0700

    outelf.c: fix a typo in previous commit
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- output/outelf.c
+++ output/outelf.c
@@ -3139,7 +3139,7 @@ static void dwarf_generate(void)
     saa_write8(pabbrev,DW_FORM_data4);
     saa_write16(pabbrev,0);     /* end of entry */
     /* Terminal zero entry */
-    saa_write8(paddrev,0);
+    saa_write8(pabbrev,0);
     abbrevlen = saalen = pabbrev->datalen;
     abbrevbuf = pbuf = nasm_malloc(saalen);
     saa_rnbytes(pabbrev, pbuf, saalen);
