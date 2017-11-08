cae5d061849ba0c7150f0089433cceeff2e602be
H. Peter Anvin <hpa@linux.intel.com>
2017-11-08 10:22:10 -0800

    More autoconf modernizations; upgrade AC_PREREQ to 2.69
    Make further autoconf rule improvements and update the required
    version of autoconf to 2.69.  That version is now 5+ years old and
    although there might be older versions which have the prerequisite
    macros they are known to have lots of bugs, and we can't really test
    them.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,7 @@
 #
 # Simple script to run the appropriate autotools from a repository.
 #
-autoheader
-autoconf
+autoreconf
 rm -rf autom4te.cache config.log config.status
 rm -f Makefile rdoff/Makefile doc/Makefile
 rm -f config.h.in config.h config/config.h
