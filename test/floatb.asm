d7498067ca06e01b73e9cca6712efa20c11b23b5
H. Peter Anvin <hpa@zytor.com>
2018-11-26 21:38:14 -0800

    BR 3392368: actually fix the documentation; add test case
    Add a mimimal test case for this bug; correct the documentation.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- test/floatb.asm
+++ test/floatb.asm
@@ -26,11 +26,30 @@
 	dq 0xd0b29a67e95dcb60
 	dw 0x4057
 
-	do 0xf.ffffff8
-	dt 0xf.ffffff8
-	dq 0xf.ffffff8
-	dd 0xf.ffffff8
-	dw 0xf.ffffff8
+	do 0xf.ffffff8p-4
+	do 0q3.7777777774p-2
+	do 0b1.1111_1111_1111_1111_1111_1111_1111_111p-1
+
+	dt 0xf.ffffff8p-4
+	dt 0q3.7777777774p-2
+	dt 0b1.1111_1111_1111_1111_1111_1111_1111_111p-1
+
+	dq 0xf.ffffff8p-4
+	dq 0q3.7777777774p-2
+	dq 0b1.1111_1111_1111_1111_1111_1111_1111_111p-1
+
+	dd 0xf.ffffff8p-4
+	dd 0q3.7777777774p-2
+	dd 0b1.1111_1111_1111_1111_1111_1111_1111_111p-1
+
+	dw 0xf.ffffff8p-4
+	dw 0q3.7777777774p-2
+	dw 0b1.1111_1111_1111_1111_1111_1111_1111_111p-1
+
+	db 0xf.ffffff8p-4
+	db 0q3.7777777774p-2
+	db 0b1.1111_1111_1111_1111_1111_1111_1111_111p-1
+
 
 	;; Way too big numbers, should overflow to +Inf
 	dd 1.0E646456955
