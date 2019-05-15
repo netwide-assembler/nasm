471120f48504300af5ace45f06980f6c049eb5b5
H. Peter Anvin (Intel) <hpa@zytor.com>
2019-05-15 13:07:21 -0700

    file.c: handle long pathnames on Windows
    Windows supports pathnames up to 32767 UTF-16 characters, but using
    the standard interfaces only up to 260 characters. Wrap the functions
    that take filenames on Windows.
    
    Clean up the compatiblity layers some more for reduced #ifdefs.
    
    Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

--- nasmlib/file.h
+++ nasmlib/file.h
@@ -58,70 +58,155 @@
 # include <sys/mman.h>
 #endif
 
-#if !defined(HAVE_ACCESS) && defined(HAVE__ACCESS)
-# define HAVE_ACCESS 1
-# define access _access
-#endif
 #ifndef R_OK
 # define R_OK 4                 /* Classic Unix constant, same on Windows */
 #endif
 
 /* Can we adjust the file size without actually writing all the bytes? */
 #ifdef HAVE__CHSIZE_S
-# define nasm_ftruncate(fd,size) _chsize_s(fd,size)
+# define os_ftruncate(fd,size)	_chsize_s(fd,size)
 #elif defined(HAVE__CHSIZE)
-# define nasm_ftruncate(fd,size) _chsize(fd,size)
+# define os_ftruncate(fd,size)	_chsize(fd,size)
 #elif defined(HAVE_FTRUNCATE)
-# define nasm_ftruncate(fd,size) ftruncate(fd,size)
+# define os_ftruncate(fd,size)	ftruncate(fd,size)
 #endif
 
 /*
- * On Win32/64, stat has a 32-bit file size but _stati64 has a 64-bit file
- * size.  Things get complicated because some of these may be macros,
- * which autoconf won't pick up on as the standard autoconf tests do
- * #undef.
+ * On Windows, we want to use _wfopen(), as fopen() has a much smaller limit
+ * on the path length that it supports. Furthermore, we want to prefix the
+ * path name with \\?\ in order to let the Windows kernel know that
+ * we are not limited to PATH_MAX characters. Thus, we wrap all the functions
+ * which take filenames...
  */
-#ifdef _stati64
-# define HAVE_STRUCT__STATI64 1
-# define HAVE__STATI64 1
-#endif
-#ifdef _fstati64
-# define HAVE__FSTATI64 1
+#ifdef _WIN32
+# include <wchar.h>
+typedef wchar_t *os_filename;
+typedef wchar_t  os_fopenflag;
+
+os_filename os_mangle_filename(const char *filename);
+static inline void os_free_filename(os_filename filename)
+{
+    nasm_free(filename);
+}
+
+# define os_fopen  _wfopen
+# define os_access _waccess
+
+/*
+ * On Win32/64, we have to use the _wstati64() function. Note that
+ * we can't use _wstat64() without depending on a needlessly new
+ * version os MSVCRT.
+ */
+
+typedef struct _stati64 os_struct_stat;
+
+# define os_stat  _wstati64
+# define os_fstat _fstati64
+
+#else  /* not _WIN32 */
+
+typedef const char *os_filename;
+typedef char os_fopenflag;
+
+static inline os_filename os_mangle_filename(const char *filename)
+{
+    return filename;
+}
+static inline void os_free_filename(os_filename filename)
+{
+    (void)filename;             /* Nothing to do */
+}
+
+# define os_fopen  fopen
+
+#if defined(HAVE_FACCESSAT) && defined(AT_EACCESS)
+static inline int os_access(os_filename pathname, int mode)
+{
+    return faccessat(AT_FDCWD, pathname, mode, AT_EACCESS);
+}
+# define os_access os_access
+#elif defined(HAVE_ACCESS)
+# define os_access access
 #endif
 
-#ifdef HAVE_STRUCT__STATI64
-typedef struct _stati64 nasm_struct_stat;
-# ifdef HAVE__STATI64
-#  define nasm_stat _stati64
-# endif
-# ifdef HAVE__FSTATI64
-#  define nasm_fstat _fstati64
-# endif
-#elif defined(HAVE_STRUCT_STAT)
-typedef struct stat nasm_struct_stat;
+#ifdef HAVE_STRUCT_STAT
+typedef struct stat os_struct_stat;
 # ifdef HAVE_STAT
-#  define nasm_stat stat
+#  define os_stat stat
 # endif
 # ifdef HAVE_FSTAT
-#  define nasm_fstat fstat
+#  define os_fstat fstat
 # endif
+#else
+struct dummy_struct_stat {
+    int st_mode;
+    int st_size;
+};
+typedef struct dummy_struct_stat os_struct_stat;
 #endif
 
-#ifndef HAVE_FILENO
-# ifdef fileno                  /* autoconf doesn't always pick up macros */
-#  define HAVE_FILENO 1
-# elif defined(HAVE__FILENO)
-#  define HAVE_FILENO 1
-#  define fileno _fileno
-# endif
+#endif  /* Not _WIN32 */
+
+#ifdef S_ISREG
+/* all good */
+#elif defined(HAVE_S_ISREG)
+/* exists, but not a macro */
+# define S_ISREG S_ISREG
+#elif defined(S_IFMT) && defined(S_IFREG)
+# define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
+#elif defined(_S_IFMT) && defined(_S_IFREG)
+# define S_ISREG(m) (((m) & _S_IFMT) == _S_IFREG)
 #endif
 
-/* These functions are utterly useless without fileno() */
-#ifndef HAVE_FILENO
-# undef nasm_fstat
-# undef nasm_ftruncate
+#ifdef fileno
+/* all good */
+#elif defined(HAVE_FILENO)
+/* exists, but not a macro */
+# define fileno fileno
+#elif defined(_fileno) || defined(HAVE__FILENO)
+# define fileno _fileno
+#endif
+
+#ifndef S_ISREG
+# undef os_stat
+# undef os_fstat
+#endif
+
+/* Disable these functions if they don't support something we need */
+#ifndef fileno
+# undef os_fstat
+# undef os_ftruncate
 # undef HAVE_MMAP
-# undef HAVE__FILELENGTHI64
+#endif
+
+/*
+ * If we don't have functional versions of these functions,
+ * stub them out so we don't need so many #ifndefs
+ */
+#ifndef os_stat
+static inline int os_stat(os_filename osfname, os_struct_stat *st)
+{
+    (void)osfname;
+    (void)st;
+    return -1;
+}
+#endif
+
+#ifndef os_fstat
+static inline int os_fstat(int fd, os_struct_stat *st)
+{
+    (void)osfname;
+    (void)st;
+    return -1;
+}
+#endif
+
+#ifndef S_ISREG
+static inline bool S_ISREG(int m)
+{
+    (void)m;
+    return false;
+}
 #endif
 
 #endif /* NASMLIB_FILE_H */
