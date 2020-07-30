0e6e0138136dd3201f55fccd5fb149051f68d7e1
H. Peter Anvin (Intel) <hpa@zytor.com>
2020-07-30 15:25:00 -0700

    nasm: don't make -L+ imply -Lw
    -Lw really is only useful to debug NASM crashes, and can hugely slow
    down the assembler. Make -L+ simply imply full verbosity; if NASM
    crashes use -Lw+ instead.
    
    Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

--- asm/listing.h
+++ asm/listing.h
@@ -1,6 +1,6 @@
 /* ----------------------------------------------------------------------- *
  *
- *   Copyright 1996-2019 The NASM Authors - All Rights Reserved
+ *   Copyright 1996-2020 The NASM Authors - All Rights Reserved
  *   See the file AUTHORS included with the NASM distribution for
  *   the specific copyright holders.
  *
@@ -134,12 +134,13 @@ extern uint64_t list_options, active_list_options;
  * is when parsing the -L option or %pragma list options, neither of
  * which is in any way performance critical.
  *
- * The character + represents ALL listing options.
+ * The character + represents ALL listing options except -Lw (flush
+ * after every line.)
  *
  * This returns 0 for invalid values, so that no bit is accessed
  * for unsupported characters.
  */
-static inline const_func uint64_t list_option_mask(unsigned char x)
+static inline const_func uint64_t list_option_mask_val(unsigned char x)
 {
     if (x >= 'a') {
         if (x > 'z')
@@ -153,13 +154,19 @@ static inline const_func uint64_t list_option_mask(unsigned char x)
         if (x > '9')
             return 0;
         x = x - '0' + 26*2;
-    } else if (x == '+') {
-        return ~UINT64_C(0);
     } else {
         return 0;
     }
 
     return UINT64_C(1) << x;
+}
+
+static inline const_func uint64_t list_option_mask(unsigned char x)
+{
+    if (x == '+')
+        return ~list_option_mask_val('w');
+    else
+        return list_option_mask_val(x);
 }
 
 static inline pure_func bool list_option(unsigned char x)
