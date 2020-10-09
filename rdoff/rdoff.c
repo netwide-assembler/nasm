netwide-assembler/nasm

commit 8dc965347ddf9caabacc4ca0441efe3a4ec32af8

rdoff: use nasm-provided safe memory allocation and I/O
We already have abort-on-error memory allocation and I/O operations in
nasmlib, so use them for rdoff as well.

Delete long-since-obsolete rdoff Mkfiles directory.

Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

rdoff/rdoff.c
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------- *
- *   
+ *
  *   Copyright 1996-2014 The NASM Authors - All Rights Reserved
  *   See the file AUTHORS included with the NASM distribution for
  *   the specific copyright holders.
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *     
+ *
  *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
  *     CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  *     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -47,13 +47,7 @@
 #include <string.h>
 #include <errno.h>
 
-#define RDOFF_UTILS
-
-#include "rdoff.h"
-
-#define newstr(str) strcpy(malloc(strlen(str) + 1),str)
-#define newstrcat(s1,s2) strcat(strcpy(malloc(strlen(s1) + strlen(s2) + 1), \
-                                       s1),s2)
+#include "rdfutils.h"
 
 /*
  * Comment this out to allow the module to read & write header record types
@@ -72,7 +66,7 @@ static memorybuffer *newmembuf(void)
 {
     memorybuffer *t;
 
-    t = malloc(sizeof(memorybuffer));
+    t = nasm_malloc(sizeof(memorybuffer));
     if (!t)
         return NULL;
 
@@ -134,8 +128,7 @@ static void membufdump(memorybuffer * b, FILE * fp)
     if (!b)
         return;
 
-    fwrite(b->buffer, 1, b->length, fp);
-
+    nasm_write(b->buffer, b->length, fp);
     membufdump(b->next, fp);
 }
 
@@ -151,7 +144,7 @@ static void freemembuf(memorybuffer * b)
     if (!b)
         return;
     freemembuf(b->next);
-    free(b);
+    nasm_free(b);
 }
 
 /* =========================================================================
@@ -232,6 +225,22 @@ const char *rdf_errors[11] = {
 };
 
 int rdf_errno = 0;
+
+/* ========================================================================
+ * Hook for nasm_error() to work
+ * ======================================================================== */
+static void rdoff_verror(int severity, const char *fmt, va_list val)
+{
+    vfprintf(stderr, fmt, val);
+
+    if ((severity & ERR_MASK) >= ERR_FATAL)
+        exit(1);
+}
+
+void rdoff_init(void)
+{
+    nasm_set_verror(rdoff_verror);
+}
 
 /* ========================================================================
    The library functions
@@ -326,7 +335,7 @@ int rdfopenhere(rdffile * f, FILE * fp, int *refcount, const char *name)
     fseek(f->fp, initpos, SEEK_SET);
     f->header_loc = NULL;
 
-    f->name = newstr(name);
+    f->name = nasm_strdup(name);
     f->refcount = refcount;
     if (refcount)
         (*refcount)++;
@@ -339,7 +348,7 @@ int rdfclose(rdffile * f)
         fclose(f->fp);
         f->fp = NULL;
     }
-    free(f->name);
+    nasm_free(f->name);
 
     return 0;
 }
@@ -510,7 +519,7 @@ void rdfheaderrewind(rdffile * f)
 
 rdf_headerbuf *rdfnewheader(void)
 {
-    rdf_headerbuf *hb = malloc(sizeof(rdf_headerbuf));
+    rdf_headerbuf *hb = nasm_malloc(sizeof(rdf_headerbuf));
     if (hb == NULL)
         return NULL;
 
@@ -589,14 +598,12 @@ int rdfwriteheader(FILE * fp, rdf_headerbuf * h)
 {
     int32_t l, l2;
 
-    fwrite(RDOFFId, 1, strlen(RDOFFId), fp);
+    nasm_write(RDOFFId, strlen(RDOFFId), fp);
 
     l = membuflength(h->buf);
     l2 = l + 14 + 10 * h->nsegments + h->seglength;
-    l = translateint32_t(l);
-    l2 = translateint32_t(l2);
-    fwrite(&l2, 4, 1, fp);      /* object length */
-    fwrite(&l, 4, 1, fp);       /* header length */
+    fwriteint32_t(l, fp);
+    fwriteint32_t(l2, fp);
 
     membufdump(h->buf, fp);
 
@@ -606,5 +613,5 @@ int rdfwriteheader(FILE * fp, rdf_headerbuf * h)
 void rdfdoneheader(rdf_headerbuf * h)
 {
     freemembuf(h->buf);
-    free(h);
+    nasm_free(h);
 }

Makefile.in
@@ -751,33 +751,41 @@ rdoff/collectn.$(O): rdoff/collectn.c config/config.h config/msvc.h \
  rdoff/collectn.h
 rdoff/hash.$(O): rdoff/hash.c config/config.h config/msvc.h config/unknown.h \
  config/watcom.h include/compiler.h include/nasmint.h rdoff/hash.h
+rdoff/iochk.$(O): rdoff/iochk.c config/config.h config/msvc.h \
+ config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
+ include/nasmlib.h
 rdoff/ldrdf.$(O): rdoff/ldrdf.c config/config.h config/msvc.h \
- config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
- include/nasmlib.h include/rdoff.h rdoff/collectn.h rdoff/ldsegs.h \
- rdoff/rdlib.h rdoff/segtab.h rdoff/symtab.h
+ config/unknown.h config/watcom.h include/compiler.h include/error.h \
+ include/nasmint.h include/nasmlib.h include/rdoff.h rdoff/collectn.h \
+ rdoff/ldsegs.h rdoff/rdfutils.h rdoff/rdlib.h rdoff/segtab.h rdoff/symtab.h
 rdoff/rdf2bin.$(O): rdoff/rdf2bin.c config/config.h config/msvc.h \
- config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
- include/nasmlib.h include/rdoff.h rdoff/rdfload.h
+ config/unknown.h config/watcom.h include/compiler.h include/error.h \
+ include/nasmint.h include/nasmlib.h include/rdoff.h rdoff/rdfload.h \
+ rdoff/rdfutils.h
 rdoff/rdfdump.$(O): rdoff/rdfdump.c config/config.h config/msvc.h \
- config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
- include/rdoff.h
+ config/unknown.h config/watcom.h include/compiler.h include/error.h \
+ include/nasmint.h include/nasmlib.h include/rdoff.h rdoff/rdfutils.h
 rdoff/rdflib.$(O): rdoff/rdflib.c config/config.h config/msvc.h \
- config/unknown.h config/watcom.h include/compiler.h include/nasmint.h
+ config/unknown.h config/watcom.h include/compiler.h include/error.h \
+ include/nasmint.h include/nasmlib.h include/rdoff.h rdoff/rdfutils.h
 rdoff/rdfload.$(O): rdoff/rdfload.c config/config.h config/msvc.h \
- config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
- include/rdoff.h rdoff/collectn.h rdoff/rdfload.h rdoff/symtab.h
+ config/unknown.h config/watcom.h include/compiler.h include/error.h \
+ include/nasmint.h include/nasmlib.h include/rdoff.h rdoff/collectn.h \
+ rdoff/rdfload.h rdoff/rdfutils.h rdoff/symtab.h
 rdoff/rdlar.$(O): rdoff/rdlar.c config/config.h config/msvc.h \
  config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
  rdoff/rdlar.h
 rdoff/rdlib.$(O): rdoff/rdlib.c config/config.h config/msvc.h \
- config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
- include/rdoff.h rdoff/rdlar.h rdoff/rdlib.h
+ config/unknown.h config/watcom.h include/compiler.h include/error.h \
+ include/nasmint.h include/nasmlib.h include/rdoff.h rdoff/rdfutils.h \
+ rdoff/rdlar.h rdoff/rdlib.h
 rdoff/rdoff.$(O): rdoff/rdoff.c config/config.h config/msvc.h \
- config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
- include/rdoff.h
+ config/unknown.h config/watcom.h include/compiler.h include/error.h \
+ include/nasmint.h include/nasmlib.h include/rdoff.h rdoff/rdfutils.h
 rdoff/rdx.$(O): rdoff/rdx.c config/config.h config/msvc.h config/unknown.h \
- config/watcom.h include/compiler.h include/nasmint.h include/rdoff.h \
- rdoff/rdfload.h rdoff/symtab.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h \
+ include/nasmlib.h include/rdoff.h rdoff/rdfload.h rdoff/rdfutils.h \
+ rdoff/symtab.h
 rdoff/segtab.$(O): rdoff/segtab.c config/config.h config/msvc.h \
  config/unknown.h config/watcom.h include/compiler.h include/nasmint.h \
  rdoff/segtab.h

Mkfiles/msvc.mak
@@ -644,30 +644,37 @@ rdoff\collectn.$(O): rdoff\collectn.c config\msvc.h config\unknown.h \
  config\watcom.h include\compiler.h include\nasmint.h rdoff\collectn.h
 rdoff\hash.$(O): rdoff\hash.c config\msvc.h config\unknown.h config\watcom.h \
  include\compiler.h include\nasmint.h rdoff\hash.h
+rdoff\iochk.$(O): rdoff\iochk.c config\msvc.h config\unknown.h \
+ config\watcom.h include\compiler.h include\nasmint.h include\nasmlib.h
 rdoff\ldrdf.$(O): rdoff\ldrdf.c config\msvc.h config\unknown.h \
- config\watcom.h include\compiler.h include\nasmint.h include\nasmlib.h \
- include\rdoff.h rdoff\collectn.h rdoff\ldsegs.h rdoff\rdlib.h \
- rdoff\segtab.h rdoff\symtab.h
+ config\watcom.h include\compiler.h include\error.h include\nasmint.h \
+ include\nasmlib.h include\rdoff.h rdoff\collectn.h rdoff\ldsegs.h \
+ rdoff\rdfutils.h rdoff\rdlib.h rdoff\segtab.h rdoff\symtab.h
 rdoff\rdf2bin.$(O): rdoff\rdf2bin.c config\msvc.h config\unknown.h \
- config\watcom.h include\compiler.h include\nasmint.h include\nasmlib.h \
- include\rdoff.h rdoff\rdfload.h
+ config\watcom.h include\compiler.h include\error.h include\nasmint.h \
+ include\nasmlib.h include\rdoff.h rdoff\rdfload.h rdoff\rdfutils.h
 rdoff\rdfdump.$(O): rdoff\rdfdump.c config\msvc.h config\unknown.h \
- config\watcom.h include\compiler.h include\nasmint.h include\rdoff.h
+ config\watcom.h include\compiler.h include\error.h include\nasmint.h \
+ include\nasmlib.h include\rdoff.h rdoff\rdfutils.h
 rdoff\rdflib.$(O): rdoff\rdflib.c config\msvc.h config\unknown.h \
- config\watcom.h include\compiler.h include\nasmint.h
+ config\watcom.h include\compiler.h include\error.h include\nasmint.h \
+ include\nasmlib.h include\rdoff.h rdoff\rdfutils.h
 rdoff\rdfload.$(O): rdoff\rdfload.c config\msvc.h config\unknown.h \
- config\watcom.h include\compiler.h include\nasmint.h include\rdoff.h \
- rdoff\collectn.h rdoff\rdfload.h rdoff\symtab.h
+ config\watcom.h include\compiler.h include\error.h include\nasmint.h \
+ include\nasmlib.h include\rdoff.h rdoff\collectn.h rdoff\rdfload.h \
+ rdoff\rdfutils.h rdoff\symtab.h
 rdoff\rdlar.$(O): rdoff\rdlar.c config\msvc.h config\unknown.h \
  config\watcom.h include\compiler.h include\nasmint.h rdoff\rdlar.h
 rdoff\rdlib.$(O): rdoff\rdlib.c config\msvc.h config\unknown.h \
- config\watcom.h include\compiler.h include\nasmint.h include\rdoff.h \
- rdoff\rdlar.h rdoff\rdlib.h
+ config\watcom.h include\compiler.h include\error.h include\nasmint.h \
+ include\nasmlib.h include\rdoff.h rdoff\rdfutils.h rdoff\rdlar.h \
+ rdoff\rdlib.h
 rdoff\rdoff.$(O): rdoff\rdoff.c config\msvc.h config\unknown.h \
- config\watcom.h include\compiler.h include\nasmint.h include\rdoff.h
+ config\watcom.h include\compiler.h include\error.h include\nasmint.h \
+ include\nasmlib.h include\rdoff.h rdoff\rdfutils.h
 rdoff\rdx.$(O): rdoff\rdx.c config\msvc.h config\unknown.h config\watcom.h \
- include\compiler.h include\nasmint.h include\rdoff.h rdoff\rdfload.h \
- rdoff\symtab.h
+ include\compiler.h include\error.h include\nasmint.h include\nasmlib.h \
+ include\rdoff.h rdoff\rdfload.h rdoff\rdfutils.h rdoff\symtab.h
 rdoff\segtab.$(O): rdoff\segtab.c config\msvc.h config\unknown.h \
  config\watcom.h include\compiler.h include\nasmint.h rdoff\segtab.h
 rdoff\symtab.$(O): rdoff\symtab.c config\msvc.h config\unknown.h \

Mkfiles/openwcom.mak
@@ -618,30 +618,37 @@ rdoff/collectn.$(O): rdoff/collectn.c config/msvc.h config/unknown.h &
  config/watcom.h include/compiler.h include/nasmint.h rdoff/collectn.h
 rdoff/hash.$(O): rdoff/hash.c config/msvc.h config/unknown.h config/watcom.h &
  include/compiler.h include/nasmint.h rdoff/hash.h
+rdoff/iochk.$(O): rdoff/iochk.c config/msvc.h config/unknown.h &
+ config/watcom.h include/compiler.h include/nasmint.h include/nasmlib.h
 rdoff/ldrdf.$(O): rdoff/ldrdf.c config/msvc.h config/unknown.h &
- config/watcom.h include/compiler.h include/nasmint.h include/nasmlib.h &
- include/rdoff.h rdoff/collectn.h rdoff/ldsegs.h rdoff/rdlib.h &
- rdoff/segtab.h rdoff/symtab.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h &
+ include/nasmlib.h include/rdoff.h rdoff/collectn.h rdoff/ldsegs.h &
+ rdoff/rdfutils.h rdoff/rdlib.h rdoff/segtab.h rdoff/symtab.h
 rdoff/rdf2bin.$(O): rdoff/rdf2bin.c config/msvc.h config/unknown.h &
- config/watcom.h include/compiler.h include/nasmint.h include/nasmlib.h &
- include/rdoff.h rdoff/rdfload.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h &
+ include/nasmlib.h include/rdoff.h rdoff/rdfload.h rdoff/rdfutils.h
 rdoff/rdfdump.$(O): rdoff/rdfdump.c config/msvc.h config/unknown.h &
- config/watcom.h include/compiler.h include/nasmint.h include/rdoff.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h &
+ include/nasmlib.h include/rdoff.h rdoff/rdfutils.h
 rdoff/rdflib.$(O): rdoff/rdflib.c config/msvc.h config/unknown.h &
- config/watcom.h include/compiler.h include/nasmint.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h &
+ include/nasmlib.h include/rdoff.h rdoff/rdfutils.h
 rdoff/rdfload.$(O): rdoff/rdfload.c config/msvc.h config/unknown.h &
- config/watcom.h include/compiler.h include/nasmint.h include/rdoff.h &
- rdoff/collectn.h rdoff/rdfload.h rdoff/symtab.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h &
+ include/nasmlib.h include/rdoff.h rdoff/collectn.h rdoff/rdfload.h &
+ rdoff/rdfutils.h rdoff/symtab.h
 rdoff/rdlar.$(O): rdoff/rdlar.c config/msvc.h config/unknown.h &
  config/watcom.h include/compiler.h include/nasmint.h rdoff/rdlar.h
 rdoff/rdlib.$(O): rdoff/rdlib.c config/msvc.h config/unknown.h &
- config/watcom.h include/compiler.h include/nasmint.h include/rdoff.h &
- rdoff/rdlar.h rdoff/rdlib.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h &
+ include/nasmlib.h include/rdoff.h rdoff/rdfutils.h rdoff/rdlar.h &
+ rdoff/rdlib.h
 rdoff/rdoff.$(O): rdoff/rdoff.c config/msvc.h config/unknown.h &
- config/watcom.h include/compiler.h include/nasmint.h include/rdoff.h
+ config/watcom.h include/compiler.h include/error.h include/nasmint.h &
+ include/nasmlib.h include/rdoff.h rdoff/rdfutils.h
 rdoff/rdx.$(O): rdoff/rdx.c config/msvc.h config/unknown.h config/watcom.h &
- include/compiler.h include/nasmint.h include/rdoff.h rdoff/rdfload.h &
- rdoff/symtab.h
+ include/compiler.h include/error.h include/nasmint.h include/nasmlib.h &
+ include/rdoff.h rdoff/rdfload.h rdoff/rdfutils.h rdoff/symtab.h
 rdoff/segtab.$(O): rdoff/segtab.c config/msvc.h config/unknown.h &
  config/watcom.h include/compiler.h include/nasmint.h rdoff/segtab.h
 rdoff/symtab.$(O): rdoff/symtab.c config/msvc.h config/unknown.h &

include/nasmlib.h
@@ -101,8 +101,10 @@ char * safe_alloc nasm_strndup(const char *, size_t);
 #define nasm_zeron(p,n) (memset((p), 0, (n)*sizeof(*(p))))
 
 /*
- * Wrapper around fwrite() which fatal-errors on output failure.
+ * Wrappers around fread()/fwrite() which fatal-errors on failure.
+ * For fread(), only use this if EOF is supposed to be a fatal error!
  */
+void nasm_read(void *, size_t, FILE *);
 void nasm_write(const void *, size_t, FILE *);
 
 /*

include/rdoff.h
@@ -1,6 +1,6 @@
 /* ----------------------------------------------------------------------- *
  *   
- *   Copyright 1996-2009 The NASM Authors - All Rights Reserved
+ *   Copyright 1996-2017 The NASM Authors - All Rights Reserved
  *   See the file AUTHORS included with the NASM distribution for
  *   the specific copyright holders.
  *
@@ -35,9 +35,8 @@
  * rdoff.h	RDOFF Object File manipulation routines header file
  */
 
-#ifndef RDOFF_RDOFF_H
-#define RDOFF_RDOFF_H 1
-
+#ifndef RDOFF_H
+#define RDOFF_H 1
 
 /*
  * RDOFF definitions. They are used by RDOFF utilities and by NASM's
@@ -167,126 +166,4 @@ struct CommonRec {
 #define SYM_GLOBAL	4
 #define SYM_IMPORT	8
 
-/*** The following part is used only by the utilities *************************/
-
-#ifdef RDOFF_UTILS
-
-/* Some systems don't define this automatically */
-#if !defined(strdup)
-extern char *strdup(const char *);
-#endif
-
-typedef union RDFHeaderRec {
-    char type;                  /* invariant throughout all below */
-    struct GenericRec g;        /* type 0 */
-    struct RelocRec r;          /* type == 1 / 6 */
-    struct ImportRec i;         /* type == 2 / 7 */
-    struct ExportRec e;         /* type == 3 */
-    struct DLLRec d;            /* type == 4 */
-    struct BSSRec b;            /* type == 5 */
-    struct ModRec m;            /* type == 8 */
-    struct CommonRec c;         /* type == 10 */
-} rdfheaderrec;
-
-struct SegmentHeaderRec {
-    /* information from file */
-    uint16_t type;
-    uint16_t number;
-    uint16_t reserved;
-    int32_t length;
-
-    /* information built up here */
-    int32_t offset;
-    uint8_t *data;                 /* pointer to segment data if it exists in memory */
-};
-
-typedef struct RDFFileInfo {
-    FILE *fp;                   /* file descriptor; must be open to use this struct */
-    int rdoff_ver;              /* should be 1; any higher => not guaranteed to work */
-    int32_t header_len;
-    int32_t header_ofs;
-
-    uint8_t *header_loc;           /* keep location of header */
-    int32_t header_fp;             /* current location within header for reading */
-
-    struct SegmentHeaderRec seg[RDF_MAXSEGS];
-    int nsegs;
-
-    int32_t eof_offset;            /* offset of the first uint8_t beyond the end of this
-                                   module */
-
-    char *name;                 /* name of module in libraries */
-    int *refcount;              /* pointer to reference count on file, or NULL */
-} rdffile;
-
-#define BUF_BLOCK_LEN 4088      /* selected to match page size (4096)
-                                 * on 80x86 machines for efficiency */
-typedef struct memorybuffer {
-    int length;
-    uint8_t buffer[BUF_BLOCK_LEN];
-    struct memorybuffer *next;
-} memorybuffer;
-
-typedef struct {
-    memorybuffer *buf;          /* buffer containing header records */
-    int nsegments;              /* number of segments to be written */
-    int32_t seglength;             /* total length of all the segments */
-} rdf_headerbuf;
-
-/* segments used by RDOFF, understood by rdoffloadseg */
-#define RDOFF_CODE	0
-#define RDOFF_DATA	1
-#define RDOFF_HEADER	-1
-/* mask for 'segment' in relocation records to find if relative relocation */
-#define RDOFF_RELATIVEMASK 64
-/* mask to find actual segment value in relocation records */
-#define RDOFF_SEGMENTMASK 63
-
-extern int rdf_errno;
-
-/* rdf_errno can hold these error codes */
-enum {
-    /* 0 */ RDF_OK,
-    /* 1 */ RDF_ERR_OPEN,
-    /* 2 */ RDF_ERR_FORMAT,
-    /* 3 */ RDF_ERR_READ,
-    /* 4 */ RDF_ERR_UNKNOWN,
-    /* 5 */ RDF_ERR_HEADER,
-    /* 6 */ RDF_ERR_NOMEM,
-    /* 7 */ RDF_ERR_VER,
-    /* 8 */ RDF_ERR_RECTYPE,
-    /* 9 */ RDF_ERR_RECLEN,
-    /* 10 */ RDF_ERR_SEGMENT
-};
-
-/* utility functions */
-int32_t translateint32_t(int32_t in);
-uint16_t translateint16_t(uint16_t in);
-char *translatesegmenttype(uint16_t type);
-
-/* RDOFF file manipulation functions */
-int rdfopen(rdffile * f, const char *name);
-int rdfopenhere(rdffile * f, FILE * fp, int *refcount, const char *name);
-int rdfclose(rdffile * f);
-int rdffindsegment(rdffile * f, int segno);
-int rdfloadseg(rdffile * f, int segment, void *buffer);
-rdfheaderrec *rdfgetheaderrec(rdffile * f);     /* returns static storage */
-void rdfheaderrewind(rdffile * f);      /* back to start of header */
-void rdfperror(const char *app, const char *name);
-
-/* functions to write a new RDOFF header to a file -
-   use rdfnewheader to allocate a header, rdfaddheader to add records to it,
-   rdfaddsegment to notify the header routines that a segment exists, and
-   to tell it how int32_t the segment will be.
-   rdfwriteheader to write the file id, object length, and header
-   to a file, and then rdfdoneheader to dispose of the header */
-
-rdf_headerbuf *rdfnewheader(void);
-int rdfaddheader(rdf_headerbuf * h, rdfheaderrec * r);
-int rdfaddsegment(rdf_headerbuf * h, int32_t seglength);
-int rdfwriteheader(FILE * fp, rdf_headerbuf * h);
-void rdfdoneheader(rdf_headerbuf * h);
-
-#endif                          /* RDOFF_UTILS */
-
-#endif                          /* RDOFF_RDOFF_H */
+#endif                          /* RDOFF_H */

nasmlib/file.c
@@ -33,6 +33,16 @@
 
 #include "file.h"
 
+void nasm_read(void *ptr, size_t size, FILE *f)
+{
+    size_t n = fread(ptr, 1, size, f);
+    if (ferror(f)) {
+        nasm_fatal(0, "unable to read input: %s", strerror(errno));
+    } else if (n != size || feof(f)) {
+        nasm_fatal(0, "fatal short read on input");
+    }
+}
+
 void nasm_write(const void *ptr, size_t size, FILE *f)
 {
     size_t n = fwrite(ptr, 1, size, f);

rdoff/Mkfiles/Makefile.dj
@@ -1,75 +1,0 @@
-# Generated automatically from Makefile.in by configure.
-#
-# Auto-configuring Makefile for RDOFF object file utils; part of the
-# Netwide Assembler
-#
-# The Netwide Assembler is copyright (C) 1996 Simon Tatham and
-# Julian Hall. All rights reserved. The software is
-# redistributable under the license given in the file "LICENSE"
-# distributed in the NASM archive.
-
-# You may need to adjust these values.
-
-prefix = /djgpp
-CC = gcc -s
-CFLAGS = -O2 -I..
-
-# You _shouldn't_ need to adjust anything below this line.
-
-exec_prefix = ${prefix}
-bindir = ${exec_prefix}/bin
-mandir = ${prefix}/man
-
-INSTALL = /usr/bin/install -c
-INSTALL_PROGRAM = ${INSTALL}
-INSTALL_DATA = ${INSTALL} -m 644
-LN_S = ln -s
-
-LDRDFLIBS = rdoff.o ../nasmlib.o symtab.o hash.o collectn.o rdlib.o segtab.o
-RDXLIBS = rdoff.o rdfload.o symtab.o hash.o collectn.o
-
-.c.o:
-	$(CC) -c $(CFLAGS) $*.c
-
-all: rdfdump ldrdf rdx rdflib rdf2bin rdf2com
-
-rdfdump: rdfdump.o rdoff.o
-	$(CC) -o rdfdump rdfdump.o rdoff.o
-
-ldrdf: ldrdf.o $(LDRDFLIBS)
-	$(CC) -o ldrdf ldrdf.o $(LDRDFLIBS)
-rdx: rdx.o $(RDXLIBS)
-	$(CC) -o rdx rdx.o $(RDXLIBS)
-rdflib: rdflib.o
-	$(CC) -o rdflib rdflib.o
-rdf2bin: rdf2bin.o $(RDXLIBS) nasmlib.o
-	$(CC) -o rdf2bin rdf2bin.o $(RDXLIBS) nasmlib.o
-rdf2com:
-	$(LN_S) rdf2bin rdf2com
-
-rdf2bin.o: rdf2bin.c
-rdfdump.o: rdfdump.c
-rdoff.o: rdoff.c rdoff.h
-ldrdf.o: ldrdf.c rdoff.h ../nasmlib.h symtab.h collectn.h rdlib.h
-symtab.o: symtab.c symtab.h hash.h
-collectn.o: collectn.c collectn.h
-rdx.o: rdx.c rdoff.h rdfload.h symtab.h
-rdfload.o: rdfload.c rdfload.h rdoff.h collectn.h symtab.h
-rdlib.o: rdlib.c rdlib.h
-rdflib.o: rdflib.c
-hash.o: hash.c hash.h
-segtab.o: segtab.c segtab.h
-
-nasmlib.o: ../nasmlib.c ../nasmlib.h ../names.c ../nasm.h
-	$(CC) -c $(CFLAGS) ../nasmlib.c
-
-clean:
-	rm -f *.o rdfdump ldrdf rdx rdflib rdf2bin rdf2com
-
-install: rdfdump ldrdf rdx rdflib rdf2bin rdf2com
-	$(INSTALL_PROGRAM) rdfdump $(bindir)/rdfdump
-	$(INSTALL_PROGRAM) ldrdf $(bindir)/ldrdf
-	$(INSTALL_PROGRAM) rdx $(bindir)/rdx
-	$(INSTALL_PROGRAM) rdflib $(bindir)/rdflib
-	$(INSTALL_PROGRAM) rdf2bin $(bindir)/rdf2bin
-	cd $(bindir); $(LN_S) rdf2bin rdf2com

commit 723fb2cc3309b9963620e8b54f3709e7a3331c57

rdoff/rdfwriteheader: Write object length and header length in correct order
Compared to 2.12.xx rdfwriteheader() writes the object length and header
length in flipped order.  Issue seems to have been introduced by commit
8dc965347ddf.

BR3392717

Signed-off-by: Ozkan Sezer <sezeroz@gmail.com>
Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

rdoff/rdoff.c
@@ -612,8 +612,8 @@ int rdfwriteheader(FILE * fp, rdf_headerbuf * h)
 
     l = membuflength(h->buf);
     l2 = l + 14 + 10 * h->nsegments + h->seglength;
-    fwriteint32_t(l, fp);
-    fwriteint32_t(l2, fp);
+    fwriteint32_t(l2, fp);      /* object length */
+    fwriteint32_t(l, fp);       /* header length */
 
     membufdump(h->buf, fp);
 