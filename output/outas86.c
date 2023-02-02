4c1cb8884bf85df0b5bb3376361702bddff1dc63
Elyes Haouas <ehaouas@noos.fr>
2023-02-02 11:24:30 +0100

    Remove useless white spaces
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>

--- output/outas86.c
+++ output/outas86.c
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------- *
- *   
+ *
  *   Copyright 1996-2017 The NASM Authors - All Rights Reserved
  *   See the file AUTHORS included with the NASM distribution for
  *   the specific copyright holders.
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *     
+ *
  *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
  *     CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  *     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -31,7 +31,7 @@
  *
  * ----------------------------------------------------------------------- */
 
-/* 
+/*
  * outas86.c	output routines for the Netwide Assembler to produce
  *		Linux as86 (bin86-0.3) object files
  */
