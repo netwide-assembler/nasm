e8b7cd44dede3f7923bb9bed52858f3f83bb1fb2
InstLatx64 <instlatx64@gmail.com>
2024-07-17 14:01:56 +0200

    fix: LATEVEX for VCVTNEPS2BF16
    -- According to Intel SDM 325462-084US, p. 1989, VCVTNEPS2BF16 has EVEX form too, so LATEVEX required
    -- only for VCVTNEPS2BF16 required explicit size-operator
    -- LATEVEX for VCVTNEPS2BF16 in avx-ne-convert.asm, avx-ne-convert-64.asm test files
    Checked with XED version: [v2024.04.01]

--- test/avx-ne-convert-64.asm
+++ test/avx-ne-convert-64.asm
@@ -11,6 +11,7 @@ BITS 64
 	vcvtneobf162ps ymm1, yword [rcx]
 	vcvtneoph2ps xmm1, oword [rbx]
 	vcvtneoph2ps ymm1, yword [rcx]
+	cpu latevex
 	vcvtneps2bf16 xmm1, xmm2
 	vcvtneps2bf16 xmm1, ymm2
 	vcvtneps2bf16 xmm1, oword [rbx]
--- test/avx-ne-convert.asm
+++ test/avx-ne-convert.asm
@@ -11,6 +11,7 @@ BITS 32
 	vcvtneobf162ps ymm1, yword [ecx]
 	vcvtneoph2ps xmm1, oword [ebx]
 	vcvtneoph2ps ymm1, yword [ecx]
+	cpu latevex
 	vcvtneps2bf16 xmm1, xmm2
 	vcvtneps2bf16 xmm1, ymm2
 	vcvtneps2bf16 xmm1, oword [ebx]
