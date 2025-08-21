1b9c2f873dfc8c59aea0f983c5795d4e3744cc91
Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
2025-08-21 19:05:00 +0200

    x86: Immediate operand longer than byte not possible
    Due to ib in the apx arithmetic instruction preprocessed implementation
    only byte size immediate operands were possible. Changing ib to i# fixes
    the issue.
    
    Signed-off-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>

--- x86/preinsns.pl
+++ x86/preinsns.pl
@@ -30,7 +30,7 @@ $$bwdq $op	rm#,imm#			[mi:	$hle o# 80# /$n i#			]	8086,SM,$lock
 $$bwdq $op	reg#?,reg#,rm#			[vrm:	evex.ndx.nf.l0.m4.o#     $02# /r	]	$apx,SM
 $$bwdq $op	reg#?,rm#,reg#			[vmr:	evex.ndx.nf.l0.m4.o#     $00# /r	]	$apx,SM
 $$wdq  $op	reg#?,rm#,sbyte#		[vmi:	evex.ndx.nf.l0.m4.o#     83 /$n ib,s	]	$apx,SM
-$$bwdq $op	reg#?,rm#,imm#			[vmi:	evex.ndx.nf.l0.m4.o#     80# /$n ib	]	$apx,SM
+$$bwdq $op	reg#?,rm#,imm#			[vmi:	evex.ndx.nf.l0.m4.o#     80# /$n i#	]	$apx,SM
 EOL
 };
 
