7e304ade609f243348976e527bf6cae68de0802c
Yongjie Sheng <sheng.yongjie@outlook.com>
2025-09-13 14:23:20 +0800

    tune VADDPH instruction and its travis test

--- travis/test/avx10.1.asm
+++ travis/test/avx10.1.asm
@@ -42,3 +42,88 @@ testcase	{ 0x62, 0xb5, 0x7d, 0x08, 0x1d, 0x4c, 0xf0, 0x01                       
 testcase	{ 0x62, 0xf5, 0x7d, 0x48, 0x1d, 0xca                                     }, { vcvtps2phx ymm1,zmm2                                         }
 testcase	{ 0x62, 0xb5, 0x7d, 0x48, 0x1d, 0x8c, 0xf0, 0x10, 0x00, 0x00, 0x00       }, { vcvtps2phx ymm1,[rax+r14*8+0x10]                             }
 testcase	{ 0x62, 0xb5, 0x7d, 0xc9, 0x1d, 0x8c, 0xf0, 0x10, 0x00, 0x00, 0x00       }, { vcvtps2phx ymm1{k1}{z},[rax+r14*8+0x10]                      }
+
+testcase        {  0x62, 0xf5, 0x74, 0x08, 0x58, 0xc9                                        }, {        {evex} VADDPH xmm1, xmm1, xmm1                                       }
+testcase        {  0x62, 0xf5, 0x74, 0x0f, 0x58, 0xc9                                        }, {        {evex} VADDPH xmm1{k7}, xmm1, xmm1                                   }
+testcase        {  0x62, 0xf5, 0x74, 0x0f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}, xmm1, oword [rax]                            }
+testcase        {  0x62, 0xb5, 0x74, 0x0f, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH xmm1{k7}, xmm1, oword [rbp+r14*2+0x8]                  }
+testcase        {  0x62, 0xf5, 0x74, 0x1f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}, xmm1, word [rax]{1to8}                       }
+testcase        {  0x62, 0xb5, 0x74, 0x1f, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH xmm1{k7}, xmm1, word [rbp+r14*2+0x8]{1to8}             }
+testcase        {  0x62, 0xf5, 0x74, 0x0f, 0x58, 0xc9                                        }, {        {evex} VADDPH xmm1{k7}, xmm1                                         }
+testcase        {  0x62, 0xf5, 0x74, 0x0f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}, oword [rax]                                  }
+testcase        {  0x62, 0xb5, 0x74, 0x0f, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH xmm1{k7}, oword [rbp+r14*2+0x8]                        }
+testcase        {  0x62, 0xf5, 0x74, 0x1f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}, word [rax]{1to8}                             }
+testcase        {  0x62, 0xb5, 0x74, 0x1f, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH xmm1{k7}, word [rbp+r14*2+0x8]{1to8}                   }
+testcase        {  0x62, 0xf5, 0x74, 0x08, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1, xmm1, oword [rax]                                }
+testcase        {  0x62, 0xf5, 0x74, 0x8f, 0x58, 0xc9                                        }, {        {evex} VADDPH xmm1{k7}{z}, xmm1, xmm1                                }
+testcase        {  0x62, 0xf5, 0x74, 0x8f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}{z}, xmm1, oword [rax]                         }
+testcase        {  0x62, 0xb5, 0x74, 0x8f, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH xmm1{k7}{z}, xmm1, oword [rbp+r14*2+0x8]               }
+testcase        {  0x62, 0xf5, 0x74, 0x9f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}{z}, xmm1, word [rax]{1to8}                    }
+testcase        {  0x62, 0xb5, 0x74, 0x9f, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH xmm1{k7}{z}, xmm1, word [rbp+r14*2+0x8]{1to8}          }
+testcase        {  0x62, 0xf5, 0x74, 0x8f, 0x58, 0xc9                                        }, {        {evex} VADDPH xmm1{k7}{z}, xmm1                                      }
+testcase        {  0x62, 0xf5, 0x74, 0x8f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}{z}, oword [rax]                               }
+testcase        {  0x62, 0xb5, 0x74, 0x8f, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH xmm1{k7}{z}, oword [rbp+r14*2+0x8]                     }
+testcase        {  0x62, 0xf5, 0x74, 0x9f, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1{k7}{z}, word [rax]{1to8}                          }
+testcase        {  0x62, 0xb5, 0x74, 0x9f, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH xmm1{k7}{z}, word [rbp+r14*2+0x8]{1to8}                }
+testcase        {  0x62, 0xb5, 0x74, 0x08, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH xmm1, xmm1, oword [rbp+r14*2+0x8]                      }
+testcase        {  0x62, 0xf5, 0x74, 0x28, 0x58, 0xc9                                        }, {        {evex} VADDPH ymm1, ymm1, ymm1                                       }
+testcase        {  0x62, 0xf5, 0x74, 0x28, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1, ymm1, yword [rax]                                }
+testcase        {  0x62, 0xb5, 0x74, 0x28, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH ymm1, ymm1, yword [rbp+r14*2+0x8]                      }
+testcase        {  0x62, 0xf5, 0x74, 0x38, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1, ymm1, word [rax]{1to16}                          }
+testcase        {  0x62, 0xb5, 0x74, 0x38, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH ymm1, ymm1, word [rbp+r14*2+0x8]{1to16}                }
+testcase        {  0x62, 0xf5, 0x74, 0x28, 0x58, 0xc9                                        }, {        {evex} VADDPH ymm1, ymm1                                             }
+testcase        {  0x62, 0xf5, 0x74, 0x28, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1, yword [rax]                                      }
+testcase        {  0x62, 0xb5, 0x74, 0x28, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH ymm1, yword [rbp+r14*2+0x8]                            }
+testcase        {  0x62, 0xf5, 0x74, 0x38, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1, word [rax]{1to16}                                }
+testcase        {  0x62, 0xb5, 0x74, 0x38, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH ymm1, word [rbp+r14*2+0x8]{1to16}                      }
+testcase        {  0x62, 0xf5, 0x74, 0x18, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1, xmm1, word [rax]{1to8}                           }
+testcase        {  0x62, 0xf5, 0x74, 0x2f, 0x58, 0xc9                                        }, {        {evex} VADDPH ymm1{k7}, ymm1, ymm1                                   }
+testcase        {  0x62, 0xf5, 0x74, 0x2f, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}, ymm1, yword [rax]                            }
+testcase        {  0x62, 0xb5, 0x74, 0x2f, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH ymm1{k7}, ymm1, yword [rbp+r14*2+0x8]                  }
+testcase        {  0x62, 0xf5, 0x74, 0x3f, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}, ymm1, word [rax]{1to16}                      }
+testcase        {  0x62, 0xb5, 0x74, 0x3f, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH ymm1{k7}, ymm1, word [rbp+r14*2+0x8]{1to16}            }
+testcase        {  0x62, 0xf5, 0x74, 0x2f, 0x58, 0xc9                                        }, {        {evex} VADDPH ymm1{k7}, ymm1                                         }
+testcase        {  0x62, 0xf5, 0x74, 0x2f, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}, yword [rax]                                  }
+testcase        {  0x62, 0xb5, 0x74, 0x2f, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH ymm1{k7}, yword [rbp+r14*2+0x8]                        }
+testcase        {  0x62, 0xf5, 0x74, 0x3f, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}, word [rax]{1to16}                            }
+testcase        {  0x62, 0xb5, 0x74, 0x3f, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH ymm1{k7}, word [rbp+r14*2+0x8]{1to16}                  }
+testcase        {  0x62, 0xb5, 0x74, 0x18, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH xmm1, xmm1, word [rbp+r14*2+0x8]{1to8}                 }
+testcase        {  0x62, 0xf5, 0x74, 0xaf, 0x58, 0xc9                                        }, {        {evex} VADDPH ymm1{k7}{z}, ymm1, ymm1                                }
+testcase        {  0x62, 0xf5, 0x74, 0xaf, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}{z}, ymm1, yword [rax]                         }
+testcase        {  0x62, 0xb5, 0x74, 0xaf, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH ymm1{k7}{z}, ymm1, yword [rbp+r14*2+0x8]               }
+testcase        {  0x62, 0xf5, 0x74, 0xbf, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}{z}, ymm1, word [rax]{1to16}                   }
+testcase        {  0x62, 0xb5, 0x74, 0xbf, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH ymm1{k7}{z}, ymm1, word [rbp+r14*2+0x8]{1to16}         }
+testcase        {  0x62, 0xf5, 0x74, 0xaf, 0x58, 0xc9                                        }, {        {evex} VADDPH ymm1{k7}{z}, ymm1                                      }
+testcase        {  0x62, 0xf5, 0x74, 0xaf, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}{z}, yword [rax]                               }
+testcase        {  0x62, 0xb5, 0x74, 0xaf, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH ymm1{k7}{z}, yword [rbp+r14*2+0x8]                     }
+testcase        {  0x62, 0xf5, 0x74, 0xbf, 0x58, 0x08                                        }, {        {evex} VADDPH ymm1{k7}{z}, word [rax]{1to16}                         }
+testcase        {  0x62, 0xb5, 0x74, 0xbf, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH ymm1{k7}{z}, word [rbp+r14*2+0x8]{1to16}               }
+testcase        {  0x62, 0xf5, 0x74, 0x08, 0x58, 0xc9                                        }, {        {evex} VADDPH xmm1, xmm1                                             }
+testcase        {  0x62, 0xf5, 0x7c, 0x48, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0, zmm0, zmm1                                       }
+testcase        {  0x62, 0xf5, 0x7c, 0x48, 0x58, 0x00                                        }, {        {evex} VADDPH zmm0, zmm0, zword [rax]                                }
+testcase        {  0x62, 0xb5, 0x7c, 0x48, 0x58, 0x84, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH zmm0, zmm0, zword [rbp+r14*2+0x8]                      }
+testcase        {  0x62, 0xf5, 0x7c, 0x38, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0, zmm0, zmm1,{rd-sae}                              }
+testcase        {  0x62, 0xf5, 0x7c, 0x48, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0, zmm1                                             }
+testcase        {  0x62, 0xf5, 0x7c, 0x48, 0x58, 0x00                                        }, {        {evex} VADDPH zmm0, zword [rax]                                      }
+testcase        {  0x62, 0xb5, 0x7c, 0x48, 0x58, 0x84, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH zmm0, zword [rbp+r14*2+0x8]                            }
+testcase        {  0x62, 0xf5, 0x7c, 0x38, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0, zmm1,{rd-sae}                                    }
+testcase        {  0x62, 0xf5, 0x7c, 0x4f, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}, zmm0, zmm1                                   }
+testcase        {  0x62, 0xf5, 0x7c, 0x4f, 0x58, 0x00                                        }, {        {evex} VADDPH zmm0{k7}, zmm0, zword [rax]                            }
+testcase        {  0x62, 0xf5, 0x74, 0x08, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1, oword [rax]                                      }
+testcase        {  0x62, 0xb5, 0x7c, 0x4f, 0x58, 0x84, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH zmm0{k7}, zmm0, zword [rbp+r14*2+0x8]                  }
+testcase        {  0x62, 0xf5, 0x7c, 0x3f, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}, zmm0, zmm1,{rd-sae}                          }
+testcase        {  0x62, 0xf5, 0x7c, 0x4f, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}, zmm1                                         }
+testcase        {  0x62, 0xf5, 0x7c, 0x4f, 0x58, 0x00                                        }, {        {evex} VADDPH zmm0{k7}, zword [rax]                                  }
+testcase        {  0x62, 0xb5, 0x7c, 0x4f, 0x58, 0x84, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH zmm0{k7}, zword [rbp+r14*2+0x8]                        }
+testcase        {  0x62, 0xf5, 0x7c, 0x3f, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}, zmm1,{rd-sae}                                }
+testcase        {  0x62, 0xf5, 0x7c, 0xcf, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}{z}, zmm0, zmm1                                }
+testcase        {  0x62, 0xf5, 0x7c, 0xcf, 0x58, 0x00                                        }, {        {evex} VADDPH zmm0{k7}{z}, zmm0, zword [rax]                         }
+testcase        {  0x62, 0xb5, 0x7c, 0xcf, 0x58, 0x84, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH zmm0{k7}{z}, zmm0, zword [rbp+r14*2+0x8]               }
+testcase        {  0x62, 0xf5, 0x7c, 0xbf, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}{z}, zmm0, zmm1,{rd-sae}                       }
+testcase        {  0x62, 0xb5, 0x74, 0x08, 0x58, 0x8c, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH xmm1, oword [rbp+r14*2+0x8]                            }
+testcase        {  0x62, 0xf5, 0x7c, 0xcf, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}{z}, zmm1                                      }
+testcase        {  0x62, 0xf5, 0x7c, 0xcf, 0x58, 0x00                                        }, {        {evex} VADDPH zmm0{k7}{z}, zword [rax]                               }
+testcase        {  0x62, 0xb5, 0x7c, 0xcf, 0x58, 0x84, 0x75, 0x08, 0x00, 0x00, 0x00          }, {        {evex} VADDPH zmm0{k7}{z}, zword [rbp+r14*2+0x8]                     }
+testcase        {  0x62, 0xf5, 0x7c, 0xbf, 0x58, 0xc1                                        }, {        {evex} VADDPH zmm0{k7}{z}, zmm1,{rd-sae}                             }
+testcase        {  0x62, 0xf5, 0x74, 0x18, 0x58, 0x08                                        }, {        {evex} VADDPH xmm1, word [rax]{1to8}                                 }
+testcase        {  0x62, 0xb5, 0x74, 0x18, 0x58, 0x4c, 0x75, 0x02                            }, {        {evex} VADDPH xmm1, word [rbp+r14*2+0x8]{1to8}                       }
