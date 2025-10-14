ab8787a1fa35c82ed98567b977026e9115d867fb
H. Peter Anvin (Intel) <hpa@zytor.com>
2025-10-14 09:43:54 -0700

    Rename "headers" directory to "templates", add .h template
    Rename the "headers" directory to "templates", and rename the files
    within as template.* with the appropriate file types so that editors
    will pick up the correct type. Add a .h file template.
    
    Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

---
+++ templates/template.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright 1996-20xx The NASM Authors - All Rights Reserved */
+
+#ifndef FILENAME_H
+#define FILENAME_H
+
+#include "compiler.h"
+
+/* Code goes here */
+
+#endif /* FILENAME_H */
