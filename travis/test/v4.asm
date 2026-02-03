0058d91bec4a70057a3bad51b0636f56637f1221
Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
2026-02-03 20:46:10 +0100

    travis: Fix tests after instruction corrections
    The UWRMSR was incorrectly encoded with operands swapped. Correct that
    mistake.
    
    Some AVX512 and AVX10.2 instructions had incorrect operands in the
    database. Correct the tests after fixing the database entries.
    
    Signed-off-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>

--- travis/test/v4.asm
+++ travis/test/v4.asm
@@ -2,12 +2,12 @@
 
 	v4fmaddps zmm0,zmm1+3,[rax]
 	v4fnmaddps zmm2,zmm3,[rax]
-	v4fmaddss zmm4,zmm5+3,[rax]
-	v4fnmaddss zmm6,zmm7+3,[rax]
+	v4fmaddss xmm4,xmm5+3,[rax]
+	v4fnmaddss xmm6,xmm7+3,[rax]
 
-	v4dpwssds zmm8,zmm9,[rax]
-	v4dpwssd zmm10,zmm11+3,[rax]
-	v4dpwssd zmm10+0,zmm11+3,[rax]
+	vp4dpwssds zmm8,zmm9,[rax]
+	vp4dpwssd zmm10,zmm11+3,[rax]
+	vp4dpwssd zmm10+0,zmm11+3,[rax]
 
 %ifdef ERROR
 	v4dpwssd zmm10+1,zmm11+3,[rax]
