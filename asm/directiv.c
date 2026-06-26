netwide-assembler/nasm

commit 1fc631a17322ce4a2db6e5bd53096f3c9b77958d

directiv.c: $-hex requires nasm_isnumstart, not just nasm_isnumchar
For identifier-escapes beginning with $a-f to work, the $ hexadecimal
prefix cannot be used, nor could it ever be used, with A-F immediately
after the $. This is the main reason the $-hex has been deprecated.

When adding the non-dollarhex mode, this got accidentally promoted to
nasm_isnumchar() instead of nasm_isnumstart(), which broke using
$-escaped identifiers in directives.

To fix this properly, it would be a good thing to add a central
identifier-skipping function.

Fixes: https://github.com/netwide-assembler/nasm/issues/223
Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

asm/directiv.c
@@ -395,7 +395,7 @@ bool process_directives(char *directive)
             value++;        /* skip escaping $ if present */
             validid = nasm_isidchar(*value);
             if (globl.dollarhex)
-                validid &= !nasm_isnumchar(*value);
+                validid &= !nasm_isnumstart(value[1]);
         } else {
             validid = nasm_isidstart(*value);
         }

test/extern.asm
@@ -11,3 +11,5 @@ global_after:
 extern_after:
 	nop
 	extern extern_after
+	extern $foo
+	jmp foo

commit 414ea824d94884b69561debe4db2ac70a831f47e

directiv.c: correct the parsing of $-symbols in directives, again
1fc631a17322 [directiv.c: $-hex requires nasm_isnumstart, not just nasm_isnumchar]

... was accidentally a commit of a wrong, broken version of the fix
(SIGMY: My fault (core dumped)).

Both the nasm_isidchar() and !nasm_isnumstart() checks need to be
applied to the *same* character, the one immediately after '$'.

Huge special thanks to ecm-pushbx for the quick testing and timely bug
report!

Reported-by: <ecm-pushbx@github.com>
Fixes: https://github.com/netwide-assembler/nasm/issues/252
Fixes: 1fc631a17322ce4a2db6e5bd53096f3c9b77958d
Fixes: 178a1b7443d5681f57241a54c0997a9b043e6d26
Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

asm/directiv.c
@@ -396,7 +396,7 @@ bool process_directives(char *directive)
             value++;        /* skip escaping $ if present */
             validid = nasm_isidchar(*value);
             if (globl.dollarhex)
-                validid &= !nasm_isnumstart(value[1]);
+                validid &= !nasm_isnumstart(*value);
         } else {
             validid = nasm_isidstart(*value);
         }